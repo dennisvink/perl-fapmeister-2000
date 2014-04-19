e1ea44d4b26f4556e8478bd1edb5653af9bd784f
Dennis <dennis@i3d.nl>
2014-04-19 10:54:46 +0200

    Improved on fapping code

--- fap.pl
+++ fap.pl
@@ -1,3 +1,3 @@
 #!/usr/bin/perl
 
-use strict; use Time::HiRes qw(usleep); print "\033[2J\nFapmeister 2000\n"; while(1) { my $naptime = int(rand(100000-20000)) + 50000; print "\033[0;0H 8=D      \n"; usleep($naptime); print "\033[0;0H 8=3D     \n"; usleep($naptime); print "\033[0;0H 8==3=D   \n"; usleep($naptime); print "\033[0;0H 8=====3=D\n"; usleep($naptime); print "\033[0;0H 8==3=D   \n"; usleep($naptime); print "\033[0;0H 8=3D     \n"; usleep($naptime); }
+use strict; use Time::HiRes qw(usleep); print "\033[2J\nFapmeister 2000\n"; while(1) { my $n = int(rand(100000-20000)) + 50000; for my $l (2,3,4,5,4,3,4,5,4,3) { print "\033[0;0H 8".('=' x ($l-2) ).($l>1?'3=':'==').'D'.(' ' x (7-$l))."\n"; usleep($n); }}
